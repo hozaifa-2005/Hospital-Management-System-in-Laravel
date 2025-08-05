e3839fd80b1231c95ad30c29915a27d418319359
hozaifa-2005 <nice2022ab@gmail.com>
2025-08-05 19:03:22 +0300

    إنشاء المشروع وجداول قاعدة البيانات مع الكونترولر والموديل

---
+++ app/Http/Controllers/LabRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LabRequest;
+use Illuminate\Http\Request;
+
+class LabRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LabRequest  $labRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LabRequest $labRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LabRequest  $labRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LabRequest $labRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LabRequest  $labRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LabRequest $labRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LabRequest  $labRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LabRequest $labRequest)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/NursingLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NursingLog;
+use Illuminate\Http\Request;
+
+class NursingLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NursingLog  $nursingLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NursingLog $nursingLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NursingLog  $nursingLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NursingLog $nursingLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NursingLog  $nursingLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NursingLog $nursingLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NursingLog  $nursingLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NursingLog $nursingLog)
+    {
+        //
+    }
+}
